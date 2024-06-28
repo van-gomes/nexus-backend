1bc0f2ec6261c342cef3d8f9c566738bb5b8a62c
van-gomes <nninha.gomes@gmail.com>
2024-06-28 20:19:48 -0300

    Create route get

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("okayyy");
+import express, {Request, Response} from 'express';
+
+const server = express();
+
+server.get('/', (request: Request, response: Response) => {
+    return response.status(200).json({message: "Stone API ok!"})
+})
+
+server.listen(5000, () => console.log("Server Stone on"));
+
