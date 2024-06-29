36c40b64c634ea3875f01679183788e789656d48
van-gomes <nninha.gomes@gmail.com>
2024-06-29 17:27:07 -0300

    Create post route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import express, {Request, Response} from 'express';
+import express, {Request, Response, request} from 'express';
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (request: Request, response: Response) => {
-    return response.status(200).json({message: "Stone API ok!"})
+    return response.status(200).json({message: 'Stone API ok!'})
+})
+
+server.post('/user', (request: Request, response: Response) => {
+    const body = request.body;
+    return response.status(201).json({message: 'Usuário criado com sucesso!'})
+    console.log(body);
 })
 
 server.listen(5000, () => console.log("Server Stone on"));
