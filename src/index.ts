2476dd2cf23bd5b123ec16099f01d20b345e409c
van-gomes <nninha.gomes@gmail.com>
2024-07-03 21:41:39 -0300

    Configure API routes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import express, {Request, Response, request} from 'express';
-import { UserController } from './controllers/UserController';
+import express, {Request, Response} from 'express';
+import { router } from './routers';
 
 const server = express();
 
-const userController = new UserController();
-
 server.use(express.json());
+server.use(router);
 
 server.get('/', (request: Request, response: Response) => {
     return response.status(200).json({message: 'Stone API ok!'})
 })
-
-server.post('/user', userController.createUser);
-server.get('/users', userController.getAllUsers);
 
 server.listen(5000, () => console.log("Server Stone on"));
 
