badf31814d4dacb5e9019d0bebee896e73270e47
van-gomes <nninha.gomes@gmail.com>
2024-07-17 23:18:00 -0300

    Create migration User

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express, {Request, Response} from 'express';
 import { router } from './routers';
 
@@ -7,8 +8,8 @@ server.use(express.json());
 server.use(router);
 
 server.get('/', (request: Request, response: Response) => {
-    return response.status(200).json({message: 'Stone API ok!'})
+    return response.status(200).json({message: 'Nexus API ok!'})
 })
 
-server.listen(5000, () => console.log("Server Stone on"));
+server.listen(5000, () => console.log("Server Nexus on"));
 
