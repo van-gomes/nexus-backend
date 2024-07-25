4d76227f3ff9b41386c8f5119c52f40090dfadba
van-gomes <nninha.gomes@gmail.com>
2024-07-24 21:56:37 -0300

    Refactor and validate routes

--- src/routers.ts
+++ src/routers.ts
@@ -6,4 +6,4 @@ export const router = Router();
 const userController = new UserController();
 
 router.post('/user', userController.createUser);
-router.get('/user', userController.getAllUsers);
+router.get('/user', userController.getUser);
